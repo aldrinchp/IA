d44ddf12d4b03ab5fc85a60c742aa2ca92728473
Aldrin Chavez <aldrinchp@gmail.com>
2025-05-14 11:10:07 -0500

    Update programa_final.py

--- programa_final.py
+++ programa_final.py
@@ -26,7 +26,7 @@ if not st.session_state.api_key:
 pregunta = st.text_area("✍️ Escribe tu pregunta sobre los Juegos Olímpicos:")
 
 # Cargar el dataset
-dataset_path = '/athlete_events_reduced_rows.csv'
+dataset_path = 'athlete_events_reduced_rows.csv'
 try:
     df = pd.read_csv(dataset_path)
 
